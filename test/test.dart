2f8fd8d0d3d2cb26c1db2a50fc58a08fb7f816ba
nightmare <mengyanshou@gmail.com>
2024-09-02 17:42:19 +0800

    Remove unused code

--- test/test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:app_channel/foundation/app_info.dart';
-import 'package:app_channel/interface/app_channel.dart';
-import 'package:signale/signale.dart';
-
-void main() async {
-  AppInfos infos = await RemoteAppChannel(port: 15000).getAppInfosV2();
-  Log.i('infos -> $infos');
-}
