f26793cf51b79d849b3552909f4177898b8aef87
nightmare <mengyanshou@gmail.com>
2024-10-29 00:27:49 +0800

    Commit

---
+++ android/src/main/java/com/nightmare/app_channel/AppChannelPlugin.java
@@ -0,0 +1,57 @@
+package com.nightmare.app_channel;
+
+import android.util.Log;
+
+import com.nightmare.applib.AppServer;
+
+import androidx.annotation.NonNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.embedding.engine.dart.DartExecutor;
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+
+
+/**
+ * AppChannelPlugin
+ */
+public class AppChannelPlugin implements FlutterPlugin, MethodCallHandler {
+    // tag
+    private static final String TAG = "AppChannelPlugin";
+    static List<FlutterEngine> flutterEngines = new ArrayList<>();
+
+    public static void addFlutterEngine(FlutterEngine flutterEngine) {
+        Log.d(TAG, "addFlutterEngine -> " + flutterEngine);
+        flutterEngines.add(flutterEngine);
+    }
+
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+        FlutterEngine flutterEngine = flutterPluginBinding.getFlutterEngine();
+        if (flutterEngines.contains(flutterEngine)) {
+            Log.d(TAG, "this engine do not need applib server");
+            return;
+        }
+        try {
+            int port = AppServer.startServerFromActivity(flutterPluginBinding.getApplicationContext());
+            Log.d(TAG, "port -> " + port);
+        } catch (Exception e) {
+            Log.d(TAG, "error -> " + e);
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+    }
+
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+    }
+}
